20eda3c5dfdf02a39e7e57c4eb12a2332d1da013
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-15 12:48:37 -0400

    Configurando la info que se verá al compartir el enlace con Open Graph

--- front-end/components/layouts/Layout.tsx
+++ front-end/components/layouts/Layout.tsx
@@ -10,19 +10,21 @@ interface Props {
     title?: string,
 }
 
+const origin = ( typeof window === 'undefined' ) ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
     return (
         <>
             <Head>
-                <title>{ title || 'Cosapi'}</title>
+                <title>{ title || 'Cosapi' }</title>
                 <link rel="shortcut icon" href="/logo.svg" />
                 <meta name='author' content='Miguel García' />
-                <meta name='description' content={`Información sobre el pokemon ${ title }`} />
-                <meta name='keywords' content={`cosapi, venezuela, pdvsa, ${ title }`} />
+                <meta name='description' content={`Información sobre Cosapi ${ '- ' + title || null }`} />
+                <meta name='keywords' content={`cosapi, venezuela, pdvsa, ${ title || null }`} />
     
-                <meta property='og:title' content={`Información sobre ${ title }`} />
-                <meta property='og:description' content={`Esta es la página sobre ${ title }`}/>
-                {/* <meta property='og:image' content={`${ origin }/img/banner.png`} /> */}
+                <meta property='og:title' content={`Información sobre Cosapi ${ '- ' + title || null }`} />
+                <meta property='og:description' content={`Esta es la página sobre Cosapi ${ '- ' + title || null }`}/>
+                <meta property='og:image' content={`${ origin }/main-banner.png`} />
             </Head>
     
             <Navbar />
