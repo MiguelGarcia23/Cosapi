2a75a5a0e402dc5d8d3bbbb3936743c16bbb0f68
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-11-29 11:26:59 -0400

    Cambiando algunas imagenes de la página. Terminando la vista de Diseño

--- front-end/components/servicios/diseño/body/Body.tsx
+++ front-end/components/servicios/diseño/body/Body.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect } from 'react';
+import Image from 'next/image';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -14,15 +15,41 @@ export const BodyDesing: FC = () => {
 
     return (
 
-        <Box data-aos='fade-up' className={ styles['design-body'] }>
+        <Box className={ styles['design-body'] }>
 
-            <Typography variant='h5' color='info.dark' className={ styles['design-intro-title']}>
-                En Cosapi entendemos que cada proyecto tiene necesidades diferentes, por lo que estamos preparados para diseñar planes a medida en función a los objetivos concretos de cada cliente, ajustándonos a los tiempos, presupuesto, etc.    
-            </Typography>
+            <Box data-aos='fade-up' style={{ marginBottom: '3.75rem' }}>
+                <Typography variant='h5' color='info.dark' className={ styles['design-intro-title']}>
+                    En Cosapi entendemos que cada proyecto tiene necesidades diferentes, por lo que estamos preparados para diseñar planes a medida en función a los objetivos concretos de cada cliente, ajustándonos a los tiempos, presupuesto, etc.    
+                </Typography>
 
-            <Typography variant='h5' color='info.dark' className={ styles['design-intro-title']}>
-                Nuestros profesionales están capacitados para establecer, según los requerimientos del proyecto, cuáles son los recursos necesarios para desarrollarlo, tomando en cuenta materiales y mano de obra. Y están en completa capacidad de asesorar sobre los productos más eficientes de acuerdo a la calidad de resultados y al costo, para asegurarse de entregar soluciones óptimas con los más altos estándares de calidad y garantizando la seguridad.  
-            </Typography>
+                <Typography variant='h5' color='info.dark' className={ styles['design-intro-title']}>
+                    Nuestros profesionales están capacitados para establecer, según los requerimientos del proyecto, cuáles son los recursos necesarios para desarrollarlo, tomando en cuenta materiales y mano de obra. Y están en completa capacidad de asesorar sobre los productos más eficientes de acuerdo a la calidad de resultados y al costo, para asegurarse de entregar soluciones óptimas con los más altos estándares de calidad y garantizando la seguridad.  
+                </Typography>
+            </Box>
+
+            <Box data-aos='fade-up' className={ styles['design-img']}>
+                <Image 
+                    src='/diagnostic-projects.png'
+                    alt='Imagen de diseño'
+                    layout='fill'
+                />
+            </Box>
+
+            <Box className={ styles['diagnostic-info-container'] }>
+
+                <Typography variant='body2' color='info.dark' sx={{ marginBottom: '2.5rem' }}>
+                    Nuestros profesionales están capacitados para establecer, según los requerimientos del proyecto, cuáles son los recursos necesarios para desarrollarlo, tomando en cuenta materiales y mano de obra. Y están en completa capacidad de asesorar sobre los productos más eficientes de acuerdo a la calidad de resultados y al costo, para asegurarse de entregar soluciones óptimas con los más altos estándares de calidad y garantizando la seguridad.    
+                </Typography>
+
+                <Typography variant='h4' color='info.dark' sx={{ marginBottom: '1rem' }}>
+                    Construyamos juntos tu próximo proyecto
+                </Typography>
+
+                <Button variant='contained' color='secondary' style={{ padding: '0.625rem 1.25rem', borderRadius: '8px' }}>
+                    <Typography variant='h5' color='info.main' textTransform='capitalize'>Agendar reunión</Typography>
+                </Button>    
+
+            </Box>
 
         </Box>
 
